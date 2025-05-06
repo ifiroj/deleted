823a9344e479d3fccddafa7ccd175338d92abf09
hyamu1 <hyamuvyamu@gmail.com>
2025-05-06 10:47:39 +0545

    feature 1 added

--- crud.py
+++ crud.py
@@ -1,3 +1,18 @@
 ## contributor : hyamu1
 
-print("Will delete this later")
+class Crud:
+    def __init__(self):
+        print("Handled CRUD Operations.")
+    
+    def create(self):
+        print("Record Created.")
+    
+    def read(self):
+        print("Record Fetched.")
+
+    def update(self):
+        print("Record Updated.")
+
+    def delete(self):
+        print("Record Deleted.")
+        
